5bf4892bec493cd4214d81f3e0c0054e40e2d23c
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-10-18 15:02:44 +0300

    add book and label class, attributes and implement methods

---
+++ classes/book.rb
@@ -0,0 +1,13 @@
+class Book < Item
+    attr_reader: 
+    def initialize(publisher, cover_state)
+        super()
+        @publisher = publisher
+        @cover_state = cover_state
+        @items = []
+    end
+
+    def can_be_archived?()
+        super || cover_state = 'bad'
+    end  
+end
--- classes/items.rb
+++ classes/items.rb
@@ -1,24 +1,25 @@
 require 'date'
 
 class Item
-  attr_reader :publish_date
-  attr_accessor :archived, :genre, :author, :source, :label
+  attr_reader :publish_date, :archived
+  attr_accessor :genre, :author, :label
 
-  def initialize(genre, author, source, label, publish_date)
+  def initialize(publish_date)
     @id = rand(1..1000)
     @genre = genre
     @author = author
-    @source = source
     @label = label
     @publish_date = publish_date
     @archived = archived
   end
 
-  def can_be_archived?
-    Date.today.year - Date.parse(@publish_date).year > 10
-  end
-
   def move_to_archive
     @archived = can_be_archived?
+  end
+
+  private 
+
+  def can_be_archived?
+    Date.today.year - Date.parse(@publish_date).year > 10
   end
 end
