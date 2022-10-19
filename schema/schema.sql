c850d0f451d61cc1e38e1bde78ddaa6875350f9b
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-10-19 12:33:11 +0300

    Add Item, Book and Author Schema Tables

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,3 +1,40 @@
+-- Item Schema
+create table Item(
+	id int primary key IDENTITY,
+	genre_id int,
+	author_id int,
+	source_id int,
+	lable_id int,
+	publish_date text,
+	archived boolean,
+	FOREIGN Key(lable_id) REFERENCES lable(id),
+	FOREIGN Key(genre_id) REFERENCES genre(id),
+	FOREIGN Key(author_id) REFERENCES author(id),
+);
+
+-- Label Schema
+create table Label(
+	id int primary key IDENTITY,
+	title varchar(20),
+	color varchar(20),
+	items varchar(256)
+)
+
+-- Book Schema
+create table Book(
+	publisher varchar(20),
+	cover_state varchar(20),
+	genre_id int,
+	author_id int,
+	source_id int,
+	lable_id int,
+	publish_date text,
+	archived boolean,
+	FOREIGN Key(lable_id) REFERENCES lable(id),
+	FOREIGN Key(genre_id) REFERENCES genre(id),
+	FOREIGN Key(author_id) REFERENCES author(id),
+)
+
 -- Author Schema
 create table author(
     id int primary key ,
